c3dbc85f29863bb4765f0c106bc36a37823c27a5
joeykorn <jkorn.d@gmail.com>
2020-06-06 11:27:14 -0700

    update vscode extensions

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,3 +1,4 @@
 code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension dracula-theme.theme-dracula
 code --install-extension vscode-icons-team.vscode-icons
