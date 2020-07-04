053ff481716a960b0d19fd115d71ad348d1ebb43
joeykorn <jkorn.d@gmail.com>
2020-07-04 11:00:41 -0700

    update hyper and vscode settings

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,4 +1,4 @@
 code --install-extension coenraads.bracket-pair-colorizer
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension dracula-theme.theme-dracula
+code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension vscode-icons-team.vscode-icons
