18aa32c6de22f5ac845d85d4312d2b085d88fa51
joeykorn <jkorn.d@gmail.com>
2021-04-22 08:49:37 -0700

    update vscode extensions

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -2,3 +2,6 @@ code --install-extension coenraads.bracket-pair-colorizer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension vscode-icons-team.vscode-icons
+code --install-extension svelte.svelte-vscode
+code --install-extension ardenivanov.svelte-intellisense
+code --install-extension fivethree.vscode-svelte-snippets
