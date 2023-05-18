b31bd08a3f81118bae72a896107282ebea155397
joeykorn <jkorn.d@gmail.com>
2023-05-18 12:45:08 -0700

    update lvim config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -109,10 +109,39 @@ linters.setup {
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/configuration/plugins/user-plugins>
 lvim.plugins = {
+  -- Treesitter
   {
-    "Mofiqul/dracula.nvim",
+    -- autoclose and autorename html tag
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  -- LSP
+  {
+    -- diagnostics, references, telescope results, quickfix and location lists
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
+  },
+  -- Colorschemes
+  {
+    -- nvim theme
+    "Mofiqul/dracula.nvim",
+  },
+  {
+    -- color highlighter
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+        RGB = true,      -- #RGB hex codes
+        RRGGBB = true,   -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true,   -- CSS rgb() and rgba() functions
+        hsl_fn = true,   -- CSS hsl() and hsla() functions
+        css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
   },
 }
 
