aeb9cb78815f34070e53ca0170681f047f4f41d4
joeykorn <jkorn.d@gmail.com>
2025-02-28 19:06:03 -0800

    astrolsp.lua config

--- config/nvim/lua/plugins/astrolsp.lua
+++ config/nvim/lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -45,6 +43,14 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      taplo = {
+        settings = {
+          formatter = {
+            indentTables = true,
+            indentEntries = true,
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
